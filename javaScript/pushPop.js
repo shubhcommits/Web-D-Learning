24212ae4ac20a822b2c4b479af8d41b69439a712
Shubham <shm.code1@gmail.com>
2023-05-17 00:15:40 +0530

    added

---
+++ javaScript/pushPop.js
@@ -0,0 +1,12 @@
+var output = [];
+var count = 1;
+function fizzBuzz(){
+    if(count<=20){
+    output.push(count);
+    count++;
+    console.log(output);
+    fizzBuzz(count);
+    }
+}
+fizzBuzz();
+//we can use pop() to remove element from  an array
