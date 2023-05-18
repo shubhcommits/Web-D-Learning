b320f80b5d62f4d9b7301a41af23b76cc6b4ecd4
Shubham <shm.code1@gmail.com>
2023-05-18 23:56:27 +0530

    added

---
+++ DOM/DOM Challenge Starting Files/index.js
@@ -0,0 +1,10 @@
+// document.getElementsByTagName("li")[2].style.color = "orange";
+// document.getElementsByClassName("btn")[0].style.color = "red ";
+// document.getElementById("title").style.color = "green";
+// document.querySelector("h1").style.color = "orange";
+// document.querySelector("#title").style.color = "green";
+// document.querySelecter("li a");
+// document.querySelector("li.item");
+// document.querySelector("list.item");  //This will output only first element 
+// document.querySelectorAll("#list.item"); //This will select all elements in an in form of  array
+document.querySelector("li a").style.color = "green";
