6541deaf428a88ac73aaa14c07d2e538eb15b79c
Shubham <shm.code1@gmail.com>
2023-05-19 00:57:12 +0530

    added

--- DOM/DOM Challenge Starting Files/index.js
+++ DOM/DOM Challenge Starting Files/index.js
@@ -14,4 +14,10 @@
 // document.querySelector("button").classList // This is used to get list of all classes in our html
 // document.querySelector("button").classList.add("invisible"); // this is used  to add class in our html;
 // document.querySelector("button").classList.toogle("invisible"); //if class(here invisible) is applied then it will removed if not then it will added
-document.querySelector("h1").classList.add("huge");
+// document.querySelector("h1").classList.add("huge");
+// document.querySelector("h1").innerHTML; //this will get output all the things inside h1
+// document.querySelector("h1").textContent;  //this will give only text inside h1
+// document.querySelector("h1").innerHTML = "<em>Good Bye<em>";
+// document.querySelector("a").attributes;  //this  used to get all attributes in it
+// document.querySelector("a").getAttribute("href");  //this is used to get the attribute link inside it.
+// document.querySelector("a").setAttribute("href","https://www.bing.com");  //this is to add attribute.
