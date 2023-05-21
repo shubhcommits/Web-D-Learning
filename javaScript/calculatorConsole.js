25a9bd959425d451ef78995b2f61c63deb10f700
Shubham <shm.code1@gmail.com>
2023-05-21 12:49:34 +0530

    added

---
+++ javaScript/calculatorConsole.js
@@ -0,0 +1,27 @@
+function add(num1,num2){
+    return num1+num2;
+}
+function multiply(num1,num2){
+    return num1*num2;
+}
+function divide(num1,num2){
+    if(num1>num2){
+        return num1/num2;
+    }else{
+        return num2/num1;
+    }
+}
+undefined
+function subtract(num1,num2){
+    if(num1>num2){
+        return num1-num2;
+    }else{
+        return num2-num1;
+    }
+}
+undefined
+function calculator(num1,num2,operator){
+    return operator(num1,num2);
+}
+calculator(5,10,divide);
+//this will give value 2
