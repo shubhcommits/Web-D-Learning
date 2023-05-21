4388501004daf6016c14de0713a427eb53b7bba1
Shubham <shm.code1@gmail.com>
2023-05-21 12:50:54 +0530

    added

---
+++ javaScript/object.js
@@ -0,0 +1,6 @@
+var houseKeeper1 = {
+    yearOfExperience: 9,
+    name: "ranjo",
+    cleaningRepertoire: ["bathroom","lobby","bedroom"]
+}
+// if we enter houseKeeper1 then it will return {yearOfExperience: 9, name: 'ranjo', cleaningRepertoire: Array(3)}
