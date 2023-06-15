e979ee3cbb58fa327a78924805b6154549d738fb
Shubham <shm.code1@gmail.com>
2023-06-15 23:38:04 +0530

    added

--- javaScript/javascript_cwh/12-arr_method2.js
+++ javaScript/javascript_cwh/12-arr_method2.js
@@ -12,13 +12,25 @@
 // console.log(num)
 
 // to sort in ascending order
-let compare = (a,b)=>{
-    return a-b
-  }
-  let num = [1,66,24,16,87,3]
-  num.sort(compare)
-  console.log(num)
-  num.reverse()
-  console.log(num)
-  
-  
+// let compare = (a,b)=>{
+//   return a-b
+// }
+// let num = [1,66,24,16,87,3]
+// num.sort(compare)
+// console.log(num)
+// num.reverse()
+ //console.log(num)
+
+/*
+ spline and slice
+
+let num = [1,66,24,16,87,3]
+num.splice(2,4,1024,2012,1025,1036)  // .splice(index frome where to remove,how many number to remove,numbers to add)
+let valuesdeleted = num.splice(2,4,1024,2012,1025,1036)
+console.log(num)
+console.log(valuesdeleted)
+*/
+
+let num = [1,66,24,16,87,3]
+let sliceitem = num.slice(2,3)
+console.log(sliceitem)
---
+++ javaScript/javascript_cwh/13-array_loops.js
@@ -0,0 +1,35 @@
+// for each loop
+/*
+let num =[3,4,6,8,6]
+num.forEach((element) => {
+  console.log(element * element)
+})
+*/
+
+//Array.from   => this convert into array
+
+/*let name ="shubham"
+let arr = Array.from(name)
+console.log(arr)
+*/
+
+// for of loop => this is used to access the elements of array
+
+/*
+let name = "shubham"
+for(let i of name){
+  console.log(i)
+}
+*/
+
+// for in loop
+let name = " shubham"
+for(let i in name){
+  console.log(i)
+}
+
+// this will print elements oF array
+let name = " shubham"
+for(let i in name[i]){
+  console.log(i)
+}
